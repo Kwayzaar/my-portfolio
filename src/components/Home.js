7460731d5d921bec7e330b5ba6202567785f841e
eric <lee.eric.03@gmail.com>
2021-07-07 19:02:19 -0600

    adds styling to home page, space for bg image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,22 @@
 import React from 'react'
 
 export default function Home() {
-  return <h1>Home Page!</h1>
+  return (
+    <main>
+      {/* <img 
+        src={}
+        alt=""
+        className="absolute object-cover w-full h-full"
+      /> */}
+      <section 
+        className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8" 
+      >
+        <h1 
+          className="text-6xl text-green-100 font-bold leading-non lg:leading-snug home-name"
+        >
+          Hi. I'm Eric
+        </h1>
+      </section>
+    </main>
+  )
 }
