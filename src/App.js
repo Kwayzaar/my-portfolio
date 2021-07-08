692a42507bca3fe4f7e837ade8ef058823b3e471
eric <lee.eric.03@gmail.com>
2021-07-07 18:37:48 -0600

    add styling to links, social icons

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import About from './components/About'
 import SinglePost from './components/SinglePost'
 import Post from './components/Post'
 import Project from './components/Project'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
       <Switch>
+        {/* include "exact" after home path so route doesn't stop at slash */}
         <Route component={Home} path='/' exact />
         <Route component={About} path='/about' />
         <Route component={SinglePost} path='/post/:slug' />
-        // post/:slug placed above /post -> no slug, render posts 
+        {/* post/:slug placed above /post -> no slug, render posts  */}
         <Route component={Post} path='/post' />
         <Route component={Project} path='/project' />
       </Switch>
