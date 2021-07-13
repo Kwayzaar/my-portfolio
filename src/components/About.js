de51dcac3e359a98012829df14cb8ab3e27b1eaa
eric <lee.eric.03@gmail.com>
2021-07-13 12:44:24 -0600

    finishes about page and styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import sanityClient from "../client.js"
+import imageUrlBuilder from "@sanity/image-url"
+import BlockContent from "@sanity/block-content-to-react"
+
+const builder = imageUrlBuilder(sanityClient)
+function urlFor(source) {
+  return builder.image(source)
+}
 
 export default function About() {
   const [author, setAuthor] = useState(null)
@@ -19,6 +26,30 @@ export default function About() {
   if(!author) return <div>Loading...</div>
   
   return (
-
+    <main className="relative">
+      <img src={""} alt={""} className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
+          <img 
+            src={urlFor(author.authorImage).url()} 
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" 
+            alt={author.name}
+          />
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="text-6xl text-green-300 mb-4">
+              Hello! I'm {" "}
+              <span className="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent 
+                blocks={author.bio} 
+                projectId="oc1i75ya"
+                dataset="production"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   )
 }
